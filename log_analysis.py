26900a5635992834152a9b607bf4eb100b736620
AmitojGill <Amitoj.s.gill@gmail.com>
2017-09-21 05:59:08 +0000

    get_most_error_per_day() and report_most_error_per_day() function added. fianl_report() funciton updated.

--- log_analysis.py
+++ log_analysis.py
@@ -3,9 +3,6 @@ This program uses python 3.0
 '''
 import psycopg2
 
-def debug_statment(text):
-	print ("DEBUG MEESSAGE: >>>   " + text)
-
 DBNAME = "news"
 
 
@@ -78,17 +75,55 @@ def get_author_views():
 def report_author_views(author_view_list):
 
 	'''
-	Objective of this function is to print top authors and their total views
+	Objective of report_author_views() function is to print top authors and their total views
 	INPUTS: author_view_list is an input in a list format with tuples as values for the list
 	OUTPUTS: It prints/desplys the authors and their view counts. 
 	'''
 
 	print ("")
-	print ("List of most popular author to the lease:")
+	print ("List from the most popular author to the least:")
 	print ("")
 
 	for author in author_view_list:
 		print (author[0] + " - " + str(author[1]) + " views")
+
+def get_most_error_per_day():
+
+	'''
+	Objective of get_most_error_per_day() function is to get most error rate per day
+	INPUTS: There are no inputs to this function
+	OUTPUTS: it returns a list of any days that has more than 1% error rate
+	VIEWS IN PostgreSQL: error_per_day, requests_per_day, and error_rate_per_day views must be created, 
+	see readme.md file for more details.
+	'''
+
+	db = psycopg2.connect(dbname = DBNAME)
+	c = db.cursor()
+
+	error_rate = '''SELECT * FROM error_rate_per_day WHERE percentage_error > 1;'''
+	
+	c.execute(error_rate)
+
+	most_error = c.fetchall()
+
+	return most_error
+	db.close()
+
+def report_most_error_per_day(most_error):
+	'''
+	Objective of report_most_error_per_day() function is to print days and error rate 
+	with more than 1% error rate per day
+	INPUTS: most_error is an input in a list format with tuples as values for the list
+	OUTPUTS: It prints/desplys the days and their error rate. 
+	'''
+
+	print(" ")
+	print("On following days error occured more than 1 %:")
+	print(" ")
+
+	for days in most_error:
+		print(str(days[0]) + " - " + str((format(days[1],'.2f'))) + "%  errors")
+
 
 
 
@@ -98,9 +133,12 @@ def final_report():
 	INPUTS: There are no inputs
 	OUTPUTS: There are no outputs
 	'''
+
 	report_top_three_articles(get_top_articlse())
 
 	report_author_views(get_author_views())
+
+	report_most_error_per_day(get_most_error_per_day())
 
 
 final_report()   
